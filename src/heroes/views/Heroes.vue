aa7f971c65d30a9b6a4d5f8f9942d1b84e4ad340
Devlin D <webmasterdevlin>
2020-09-02 13:54:05 +0200

    updated

--- src/heroes/views/Heroes.vue
+++ src/heroes/views/Heroes.vue
@@ -2,15 +2,20 @@
   <div class="container-fluid">
     <h1>Heroes</h1>
     <section>
-      <!-- <div v-if="heroes.length > 0">
-        <div class="card mt-3" style="width: auto;" v-for="hero in heroes" :key="hero.id">
+      <div v-if="heroes.length > 0">
+        <div
+          class="card mt-3"
+          style="width: auto;"
+          v-for="hero in heroes"
+          :key="hero.id"
+        >
           <div class="card-header">
             <h3 class="card-title">{{ hero.firstName }} {{ hero.lastName }}</h3>
             <h5 class="card-subtitle mb-2 text-muted">{{ hero.house }}</h5>
             <p class="card-text">{{ hero.knownAs }}</p>
           </div>
         </div>
-      </div>-->
+      </div>
     </section>
   </div>
 </template>
@@ -23,8 +28,13 @@ export default {
 
   mounted() {
     this.getHeroesAction();
-    // TODO: GET HTTP REQUEST X
-    // TODO: Action Vuex
+  },
+
+  computed: {
+    ...mapGetters("heroModule", {
+      heroes: "heroes",
+      isLoading: "isLoading",
+    }),
   },
 
   methods: {
