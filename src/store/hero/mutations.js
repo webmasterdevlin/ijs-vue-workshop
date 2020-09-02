a2e934a5bab15b8d92158863d4f367df6e3db069
Devlin D <webmasterdevlin>
2020-09-02 13:33:10 +0200

    updated

---
+++ src/store/hero/mutations.js
@@ -0,0 +1,11 @@
+import * as types from "./types";
+
+const mutations = {
+  [types.GET_HEROES](state, heroes) {
+    state.heroes = heroes;
+  },
+
+  [types.ISLOADING_HERO](state, toggle) {
+    state.isLoading = toggle;
+  },
+};
