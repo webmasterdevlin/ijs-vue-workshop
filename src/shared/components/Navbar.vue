bad40be4e3b3b47b1ca127bba17e0d91f91179a2
Devlin D <webmasterdevlin>
2020-09-02 12:34:36 +0200

    updated

---
+++ src/shared/components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav
+    class="navbar navbar-expand-lg 
+        navbar-light bg-light d-flex justify-content-start"
+  >
+    <span class="navbar-brand">
+      LOGO
+    </span>
+  </nav>
+</template>
