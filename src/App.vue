9fcef0a067e0a7f99cdf4b325d659b3314da40df
Devlin D <webmasterdevlin>
2020-09-02 13:59:55 +0200

    updated

--- src/App.vue
+++ src/App.vue
@@ -22,12 +22,6 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/bootstrap/dist/css/bootstrap.css";
+/* @import "../node_modules/bootstrap-icons"; */
 </style>
