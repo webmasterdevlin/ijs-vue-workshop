2a9f3d80141bed959df37563541c745c1ceea535
Devlin D <webmasterdevlin>
2020-09-02 13:07:44 +0200

    updated

--- src/heroes/views/Heroes.vue
+++ src/heroes/views/Heroes.vue
@@ -2,7 +2,7 @@
   <div class="container-fluid">
     <h1>Heroes</h1>
     <section>
-      <div v-if="heroes.length > 0">
+      <!-- <div v-if="heroes.length > 0">
         <div class="card mt-3" style="width: auto;" v-for="hero in heroes" :key="hero.id">
           <div class="card-header">
             <h3 class="card-title">{{ hero.firstName }} {{ hero.lastName }}</h3>
@@ -10,7 +10,7 @@
             <p class="card-text">{{ hero.knownAs }}</p>
           </div>
         </div>
-      </div>
+      </div>-->
     </section>
   </div>
 </template>
