5e16523e2357a236fd8ba350a34a175b60e061fb
Devlin D <webmasterdevlin>
2020-09-02 15:08:28 +0200

    updated

--- src/heroes/views/Heroes.vue
+++ src/heroes/views/Heroes.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="container-fluid">
     <h1>Heroes</h1>
-    <section>
+    <div
+      v-if="isLoading"
+      style="display: flex; flex-direction: row; justify-content: center;"
+    >
+      <div
+        class="spinner-border"
+        style="width: 6rem; height: 6rem; color: purple;"
+        role="status"
+      >
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+
+    <section v-else>
       <div v-if="heroes.length > 0">
         <div
           class="card mt-3"
